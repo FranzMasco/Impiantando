ee4a480efe9fced96434f4fec290ad59c054eeac
FranzMasco <francescomascotti@gmail.com>
2022-05-19 13:17:44 +0200

    Fixato GET che non funzionava appropriatamente e aggiunto il POST

--- WebApplication/models/facilities.js
+++ WebApplication/models/facilities.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const facilities = mongoose.Schema({
-    nome: String,
-    descrizione: String,
+    name: String,
+    description: String,
+    id_s_c: mongoose.Schema.Types.ObjectId,
 },{collection: 'impianto'});
 
 module.exports = mongoose.model("Facilities", facilities);
--- WebApplication/routes/sport_centers.js
+++ WebApplication/routes/sport_centers.js
@@ -78,16 +78,44 @@ router.post('/sport_centers', async (req, res) => {
     res.location("/api/v1/sport_centers/" + sport_center_id).status(201).send();    
 });
 
-router.get('/sport_centers/:id/sport_facilities', async (req, res) => {
-    let facilities = await Facilities.findByID(req.params.id);
+router.get('/sport_centers/:id', async (req, res) => {
+    let facilities = await Facilities.find({id_s_c:req.params.id});
     let response = facilities.map( (facility) => {
         return {
-            self: "/api/v1/sport_centers/:id/sport_facilities" + facility.id,
+            self: "/api/v1/sport_centers/:id/sport_facilities/" + facility.id,
             name: facility.name,
             description: facility.description,
+            id_s_c:facility.id_s_c,
         };
     });
     res.status(200).json(response);
+});
+
+
+router.post('/sport_centers/:id', async (req, res) => {
+    let facility_name = req.body.name;
+    let facility_description = req.body.description;
+    let facility_id_s_c = req.body.id_s_c;
+
+    //Check if a facility already exists
+    const facilitiesExists = await Facilities.findOne({ 'name': facility_name, 'description': facility_description,'id_s_c':facility_id_s_c}).select("name").lean();
+    if (facilitiesExists) {res.status(409).send('facility already exists'); return;}
+
+    let facility = new Facilities({
+        name: facility_name,
+        description: facility_description,
+        id_s_c: facility_id_s_c,
+    });
+
+    await facility.save();
+
+    let sport_facility_id = facility.id;
+
+    /**
+     * Link to the newly created resource is returned in the Location header
+     * https://www.restapitutorial.com/lessons/httpmethods.html
+    */
+    res.location("/api/v1/sport_centers/:id/sport_facilities" + sport_facility_id).status(201).send();    
 });
 
 
