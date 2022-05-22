a0a299bffb9457099c4f479af693f13c8f654ed5
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2022-05-22 16:45:49 +0200

    Rimozione modelli non importanti

--- WebApplication/models/utente_prova.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const schema = mongoose.Schema({
-	username: String,
-    password: String
-},{collection: 'utente_prova'})
-
-
-module.exports = mongoose.model("Utente_prova", schema)
