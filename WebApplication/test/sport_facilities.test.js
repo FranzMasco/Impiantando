e538288f37298b859b7c914469a87b38857cd37d
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2022-05-31 21:13:37 +0200

    Creazione test metodo get di sport_centers e sport_facilities

---
+++ WebApplication/test/sport_facilities.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const app = require('../app');
+const mongoose = require('mongoose');
+
+describe('GET /api/v2/sport_facilities', () => {
+    let connection;
+    
+    beforeAll( async() => {
+        jest.setTimeout(8000);
+        connection = await mongoose.connect(process.env.DB_URL);
+    });
+    afterAll( () => { 
+        mongoose.connection.close(true);
+    });
+
+
+    //var token = jwt.sign({username: 'antonio.gialli',id: '628501997debfcb7b90be07e'}, process.env.SUPER_SECRET, {expiresIn: 86400});
+
+    test('GET /api/v2/sport_facilities should respond with status 200', async () => {
+        request(app).get('/api/v2/sport_facilities').expect(200);
+    })
+})
