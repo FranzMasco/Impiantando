5919560193dd351b3b56dc266017aab9a7755970
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2022-06-01 11:26:13 +0200

    Aggiunta test metodo POST API sport_facilities

--- WebApplication/test/sport_facilities.test.js
+++ WebApplication/test/sport_facilities.test.js
@@ -15,9 +15,22 @@ describe('GET /api/v2/sport_facilities', () => {
     });
 
 
-    //var token = jwt.sign({username: 'antonio.gialli',id: '628501997debfcb7b90be07e'}, process.env.SUPER_SECRET, {expiresIn: 86400});
+    var token = jwt.sign({username: 'antonio.gialli',id: '628501997debfcb7b90be07e'}, process.env.SUPER_SECRET, {expiresIn: 86400});
 
     test('GET /api/v2/sport_facilities should respond with status 200', async () => {
         request(app).get('/api/v2/sport_facilities').expect(200);
+    })
+
+    test('POST /api/v2/sport_facilities without a name specified', () =>{
+        return request(app).post('/api/v2/sport_facilities')
+        .set('x-access-token', token).set('Accept', 'application/json')
+        .expect(400, { error: 'Name not specified'});
+    });
+
+    test('POST /api/v2/sport_facilities of a sport facility already existing in a sport center', () => {
+        return request(app).post('/api/v2/sport_facilities')
+        .set('x-access-token', token).set('Accept', 'application/json')
+        .send({name: 'Piscina', id_s_c: '628501997debfcb7b90be07f'})
+        .expect(409);
     })
 })
