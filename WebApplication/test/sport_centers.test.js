47f585ee679bf4a993c4bd92ebaf783ebcbba7be
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2022-06-01 21:10:14 +0200

    Aggiunta test metodi GET di API sport_centers

--- WebApplication/test/sport_centers.test.js
+++ WebApplication/test/sport_centers.test.js
@@ -18,6 +18,22 @@ describe('GET /api/v2/sport_centers', () => {
     //var token = jwt.sign({username: 'antonio.gialli',id: '628501997debfcb7b90be07e'}, process.env.SUPER_SECRET, {expiresIn: 86400});
 
     test('GET /api/v2/sport_centers should respond with status 200', async () => {
-        request(app).get('/api/v2/sport_centers').expect(200);
+        return request(app).get('/api/v2/sport_centers').expect(200);
+    })
+
+    test('GET /api/v2/sport_centers/:id should respond with status 200', async () => {
+        return request(app).get('/api/v2/sport_centers/628501997debfcb7b90be07f').expect(200);
+    })
+
+    test('GET /api/v2/sport_centers/:id/sport_facilities should respond with status 200', async () => {
+        return request(app).get('/api/v2/sport_centers/628501997debfcb7b90be07f/sport_facilities').expect(200);
+    })
+
+    test('GET /api/v2/sport_centers/:id/courses should respond with status 200', async () => {
+        return request(app).get('/api/v2/sport_centers/628501997debfcb7b90be07f/courses').expect(200);
+    })
+
+    test('GET /api/v2/sport_centers/:id/managers should respond with status 200', async () => {
+        return request(app).get('/api/v2/sport_centers/628501997debfcb7b90be07f/managers').expect(200);
     })
 })
