4be01da97e2de2e60dc32c37ed7cf0c64b0ab8a9
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2022-06-01 13:55:11 +0200

    Aggiunta test metodi GET specifici di API sport_facilities

--- WebApplication/test/sport_facilities.test.js
+++ WebApplication/test/sport_facilities.test.js
@@ -15,11 +15,23 @@ describe('GET /api/v2/sport_facilities', () => {
         mongoose.connection.close(true);
     });
 
-
+    var id_post="";
     var token = jwt.sign({username: 'antonio.gialli',id: '628501997debfcb7b90be07e'}, process.env.SUPER_SECRET, {expiresIn: 86400});
 
     test('GET /api/v2/sport_facilities should respond with status 200', async () => {
-        request(app).get('/api/v2/sport_facilities').expect(200);
+        return request(app).get('/api/v2/sport_facilities').expect(200);
+    })
+
+    test('GET /api/v2/sport_facilities/:id should respond with status 200', async () => {
+        return request(app).get('/api/v2/sport_facilities/628371f870f00f63080bd17c').expect(200);
+    })
+
+    test('GET /api/v2/sport_facilities/:id/courses should respond with status 404 if the sport facility does not exist', async () => {
+        return request(app).get('/api/v2/sport_facilities/628371f870f00f63080bd17b/courses').expect(404);
+    })
+
+    test('GET /api/v2/sport_facilities/:id/courses should respond with status 200', async () => {
+        return request(app).get('/api/v2/sport_facilities/628371f870f00f63080bd17c/courses').expect(200);
     })
 
     test('POST /api/v2/sport_facilities without a name specified', () =>{
@@ -36,10 +48,10 @@ describe('GET /api/v2/sport_facilities', () => {
     })
 
     test('POST /api/v2/sport_facilities of a sport facility that does not exist', () => {
-        return request(app).post('/api/v2/sport_facilities')
+        let post = request(app).post('/api/v2/sport_facilities')
         .set('x-access-token', token).set('Accept', 'application/json')
-        .send({name: 'Test', description: 'Sport facility inserita attraverso test', id_s_c: '628501997debfcb7b90be07f'})
-        .expect(201);
+        .send({name: 'Test', description: 'Sport facility inserita attraverso test', id_s_c: '628501997debfcb7b90be07f'});
+        return post.expect(201);
     })
 
     test('DELETE /api/v2/sport_facilities/:id of a sport facility that does not exist', () => {
@@ -49,7 +61,7 @@ describe('GET /api/v2/sport_facilities', () => {
     })
 
     /*test('DELETE /api/v2/sport_facilities/:id of a sport facility that exists', () => {
-        //let sport_facility = facilities.findOne({'name': 'Test'});
+        let sport_facility = facilities.findOne({'name': 'Test'});
         return request(app).delete('/api/v2/sport_facilities/'+sport_facility.id)
         .set('x-access-token', token).set('Accept', 'application/json')
         .expect(204);
