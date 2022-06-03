c5b1865a0e51651b4061084154f0e5c922f98e73
PietroFronzaUniTn <pietro.fronza@studenti.unitn.it>
2022-06-04 00:05:23 +0200

    Cambiamenti al codice dei test relativi ad API sport_facilities dei metodi GET e POST

--- WebApplication/test/sport_facilities.test.js
+++ WebApplication/test/sport_facilities.test.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const app = require('../app');
 const mongoose = require('mongoose');
 const Facilities = require('../models/facilities');
+const { response } = require('../app');
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -10,7 +11,6 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
   
-
 describe('/api/v2/sport_facilities', () => {
     let connection;
     
@@ -22,59 +22,149 @@ describe('/api/v2/sport_facilities', () => {
         mongoose.connection.close(true);
     });
 
+    //Correct token
     var token = jwt.sign({username: 'antonio.gialli',id: '628501997debfcb7b90be07e'}, process.env.SUPER_SECRET, {expiresIn: 86400});
-    describe('GET methods tests', () => {
-        test('GET /api/v2/sport_facilities should respond with status 200', async () => {
-            return request(app).get('/api/v2/sport_facilities').expect(200);
-        })
-    
-        test('GET /api/v2/sport_facilities/:id should respond with status 200', async () => {
-            return request(app).get('/api/v2/sport_facilities/628371f870f00f63080bd17c').expect(200);
-        })
-    
-        test('GET /api/v2/sport_facilities/:id/courses should respond with status 404 if the sport facility does not exist', async () => {
-            return request(app).get('/api/v2/sport_facilities/628371f870f00f63080bd17b/courses').expect(404);
-        })
-    
-        test('GET /api/v2/sport_facilities/:id/courses should respond with status 200', async () => {
-            return request(app).get('/api/v2/sport_facilities/628371f870f00f63080bd17c/courses').expect(200);
-        })
-    })
+
+    //Invalid token
+    var token_not_valid = "This token is not valid";
+
+    //stored information
+    var sport_facility_id;
+    var sport_facility_name;
+    var sport_facility1_id;
+    var sport_facility1_name;
 
     describe('POST method tests', () => {
-        test('POST /api/v2/sport_facilities without a name specified', () =>{
-            return request(app).post('/api/v2/sport_facilities')
-            .set('x-access-token', token).set('Accept', 'application/json')
-            .expect(400, "Bad input - missing required information");
+
+        //POST with missing required parameter
+        test('POST /api/v2/sport_facilities with required parameters not specified. Should respond with status 400', () => {
+            return request(app)
+                .post('/api/v2/sport_facilities')
+                .set('x-access-token', token)
+                .set('Accept', 'application/json')
+                .expect(400);
         });
-    
-        test('POST /api/v2/sport_facilities of a sport facility already existing in a sport center', () => {
-            return request(app).post('/api/v2/sport_facilities')
-            .set('x-access-token', token).set('Accept', 'application/json')
-            .send({name: 'Piscina', id_s_c: '628501997debfcb7b90be07f'})
-            .expect(409);
-        })
+
+        //POST without token
+        test('POST /api/v2/sport_facilities without JSON web token. Should respond with status 401', () => {
+            return request(app)
+                .post('/api/v2/sport_facilities')
+                .set('Accept', 'application/json')
+                .expect(401);
+        });
+
+        //POST with invalid token
+        test('POST /api/v2/sport_facilities with required parameters not specified. Should respond with status 400', () => {
+            return request(app)
+                .post('/api/v2/sport_facilities')
+                .set('x-access-token', token_not_valid)
+                .set('Accept', 'application/json')
+                .expect(403);
+        });
+
+        //POST with valid data
+        //store _id and another information about the created resource for future test cases
+        test('POST /api/v2/sport_facilities with correct data. Should respond with status 201', () => {
+
+            //Prepare a random name to avoid conflicts
+            sport_facility_name = "test"+getRandomIntInclusive(0,1000000)+getRandomIntInclusive(0,1000000);
+
+            return request(app)
+                .post('/api/v2/sport_facilities')
+                .set('x-access-token', token)
+                .set('Accept', 'application/json')
+                .send({
+                    name: sport_facility_name, 
+                    description: 'Sport facility inserita attraverso test', 
+                    id_s_c: '628501997debfcb7b90be07f'
+                }) 
+                .expect(201)
+                .then((response) => {
+                    //Get ID of the sport manager just created from location header field
+                    //and store it for later test cases
+                    let locationHeaderField = response.headers["location"];
+                    sport_facility_id = locationHeaderField.substring(locationHeaderField.lastIndexOf('/')+1);
+                });
+        });
+
+        //POST of a resource which already exists
+        test('POST /api/v2/sport_facilities with correct data but the specified sport_facility already exists. Should respond with status 409', () => {
+            return request(app)
+                .post('/api/v2/sport_facilities')
+                .set('x-access-token', token)
+                .set('Accept', 'application/json')
+                .send({
+                    name: sport_facility_name, 
+                    description: 'Sport facility inserita attraverso test', 
+                    id_s_c: '628501997debfcb7b90be07f'
+                }) 
+                .expect(409);
+        });
+
+        //POST with valid data --> create a new instance for later test cases
+        test('POST /api/v2/sport_facilities with correct data. Should respond with status 201', () => {
+
+            //Prepare a random name to avoid conflicts
+            var new_sport_facility_name = "test"+getRandomIntInclusive(0,1000000)+getRandomIntInclusive(0,1000000);
+
+            return request(app)
+                .post('/api/v2/sport_centers')
+                .set('x-access-token', token)
+                .set('Accept', 'application/json')
+                .send({
+                    name: new_sport_facility_name, 
+                    description: 'Sport facility inserita attraverso test', 
+                    id_s_c: '628501997debfcb7b90be07f'
+                })
+                .expect(201);
+        });
     })
 
-    describe('POST method funzionante', () => {
-        let test_facility_post
-        beforeAll( async() => {
-            let test_facility_post_name = 'Test'+getRandomIntInclusive(0,1000000)+''+getRandomIntInclusive(0,1000000);
-            
-            test_facility_post = {
-                name: test_facility_post_name, description: 'Sport facility inserita attraverso test', id_s_c: '628501997debfcb7b90be07f'
-            };
-            //console.log(test_facility_post)
+    describe('GET methods tests', () => {
+
+        //GET all the resources
+        test('GET /api/v2/sport_facilities should respond with status 200', async () => {
+            return request(app)
+                .get('/api/v2/sport_facilities')
+                .expect(200)
+                .then((response) => {
+                    //The length of the response should be at least two due to previous posts
+                    expect(response.body.length).toBeGreaterThanOrEqual(2);
+            });
         })
         
-        test('POST /api/v2/sport_facilities of a sport facility that does not exist', () => {
-            let post = request(app).post('/api/v2/sport_facilities')
-            .set('x-access-token', token).set('Accept', 'application/json')
-            .send(test_facility_post);
-            return post.expect(201);
+        //GET specific resource with not valid ID. Should respond with status 404
+        test('GET /api/v2/sport_facilities/:id with not valid ID. Should respond with status 404.', async () => {
+            return request(app)
+                .get('/api/v2/sport_facilities/notValidID')
+                .expect(404);
+        })
+
+        //GET specific resource with valid ID. Should respond with status 200 and with the data of the previously created resource
+        test('GET /api/v2/sport_facilities/:id with valid ID. Should respond with status 200 and with the data of the previously created resource', async () => {
+            return request(app)
+                .get('/api/v2/sport_facilities/'+sport_facility_id)
+                .expect(200)
+                .then((response) => {
+                    expect(response.body.name).toBe(sport_facility_name);
+                });
         })
     })
 
+    
+
+    test('GET /api/v2/sport_facilities/:id should respond with status 200', async () => {
+        return request(app).get('/api/v2/sport_facilities/628371f870f00f63080bd17c').expect(200);
+    })
+
+    test('GET /api/v2/sport_facilities/:id/courses should respond with status 404 if the sport facility does not exist', async () => {
+        return request(app).get('/api/v2/sport_facilities/628371f870f00f63080bd17b/courses').expect(404);
+    })
+
+    test('GET /api/v2/sport_facilities/:id/courses should respond with status 200', async () => {
+        return request(app).get('/api/v2/sport_facilities/628371f870f00f63080bd17c/courses').expect(200);
+    })
+        
     describe('DELETE methods test', () => {
         test('DELETE /api/v2/sport_facilities/:id of a sport facility that does not exist', () => {
             return request(app).delete('/api/v2/sport_facilities/628371f870f00f63080bd17b')
