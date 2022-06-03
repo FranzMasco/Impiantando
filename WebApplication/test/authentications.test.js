ed9cd7e03d6330b30262dca77569e7aca65f7057
Stefano Genetti <stefano.genetti@studenti.unitn.it>
2022-06-03 19:15:03 +0200

    Authentication manager user test

--- WebApplication/test/authentications.test.js
+++ WebApplication/test/authentications.test.js
@@ -88,4 +88,82 @@ describe('/api/v2/authentications', () => {
               });
         });
     })
+
+    describe('Manager authentication tests', () => {
+
+        //Mock function
+        let responseSpy;
+        beforeAll( () => {
+            const Responsabile = require('../models/manager_user');
+            responseSpy = jest.spyOn(Responsabile, 'findOne').mockImplementation((input) => {
+                if(input.username=="test"){
+                return {
+                        name: "prova",
+                        surname: "prova",
+                        email: "prova",
+                        birth_date: "2000-08-20",
+                        username: "test",
+                        password: "password"
+                    };
+                }
+            });
+        });
+
+        afterAll(async () => {
+            responseSpy.mockRestore();
+        });
+
+        //Authentication with correct username and password
+        test('POST /api/v2/authentications/responsabile with correct username and password. Should respond with status 200. Check BODY parameters: [success: true, user: responsabile, username: test]', async () => {
+            return request(app)
+              .post('/api/v2/authentications/responsabile')
+              .set('Accept', 'application/json')
+              .send({
+                  username: "test",
+                  password: "password"
+              })
+              .expect(200)
+              .then((response) => {
+                //Check response
+                expect(response.body.success).toBe(true);
+                expect(response.body.user).toBe("responsabile");
+                expect(response.body.username).toBe("test");
+              });
+        });
+
+        //Authentication with wrong username
+        test('POST /api/v2/authentications/responsabile with wrong username. Should respond with status 404. Check BODY parameters: [success: false, username: false]', async () => {
+            return request(app)
+              .post('/api/v2/authentications/responsabile')
+              .set('Accept', 'application/json')
+              .send({
+                  username: "testWrong",
+                  password: "password"
+              })
+              .expect(404)
+              .then((response) => {
+                //Check response
+                expect(response.body.success).toBe(false);
+                expect(response.body.username).toBe(false);
+              });
+        });
+
+        //Authentication with wrong password and correct username
+        test('POST /api/v2/authentications/responsabile with wrong password and correct username. Should respond with status 404. Check BODY parameters: [success: false, username: true, password: false]', async () => {
+            return request(app)
+              .post('/api/v2/authentications/responsabile')
+              .set('Accept', 'application/json')
+              .send({
+                  username: "test",
+                  password: "passwordWrong"
+              })
+              .expect(404)
+              .then((response) => {
+                //Check response
+                expect(response.body.success).toBe(false);
+                expect(response.body.username).toBe(true);
+                expect(response.body.password).toBe(false);
+              });
+        });
+    })
 });
